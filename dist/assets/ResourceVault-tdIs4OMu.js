ec73b430142713169de2d9bf8dc808bc0fb242b0
Pooja675 <poojasinha675@gmail.com>
2025-09-25 10:22:44 -0400

    fixed_tabtitle_favicon

--- dist/assets/ResourceVault-tdIs4OMu.js
+++ dist/assets/ResourceVault-tdIs4OMu.js
@@ -1,4 +1,4 @@
-import{c as v,r as s,j as e,A as D,m as S,B as Y,X as A}from"./index-DOHAn1wV.js";/**
+import{c as v,r as s,j as e,A as D,m as S,B as Y,X as A}from"./index-CsCixfXi.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
